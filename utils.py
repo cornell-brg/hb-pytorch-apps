0103ca85a6f753778fb324bfef58ac051d45d1f4
Bandhav Veluri <bandhav.veluri00@gmail.com>
2020-04-15 18:05:19 -0700

    Add a common util file

---
+++ utils.py
@@ -0,0 +1,22 @@
+import argparse
+
+ATOL = 1e-5
+RTOL = 1e-5
+
+def parse_arguments():
+    parser = argparse.ArgumentParser(formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+    parser.add_argument('--nepoch', default=30, type=int,
+                        help="number of training epochs")
+    parser.add_argument('--hammerblade', default=False, action='store_true',
+                        help="run MLP MNIST on HammerBlade")
+    parser.add_argument("--verbosity", default=False, action='store_true',
+                        help="increase output verbosity")
+    parser.add_argument("--print-internal", default=False, action='store_true',
+                        help="print internal buffers")
+    parser.add_argument("--dry", default=False, action='store_true',
+                        help="dry run")
+    parser.add_argument("--save-model", default=False, action='store_true',
+                        help="save trained model to file")
+    parser.add_argument('--save-filename', default="trained_model", type=str,
+                        help="filename of the saved model")
+    return parser.parse_args()

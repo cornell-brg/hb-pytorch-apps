8cbbf41a0d0cf1b084e0cfc9a7a385c6a38437f8
Bandhav Veluri <bandhav.veluri00@gmail.com>
2020-04-16 00:26:50 -0700

    Train argparser

--- utils.py
+++ utils.py
@@ -16,3 +16,21 @@ def argparse_inference():
     parser.add_argument('--filename', default="trained_model", type=str,
                         help="filename of the saved model")
     return parser.parse_args()
+
+def argparse_training():
+    parser = argparse.ArgumentParser(formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+    parser.add_argument('--nepoch', default=30, type=int,
+                        help="number of training epochs")
+    parser.add_argument('--hammerblade', default=False, action='store_true',
+                        help="run MLP MNIST on HammerBlade")
+    parser.add_argument("--verbosity", default=False, action='store_true',
+                        help="increase output verbosity")
+    parser.add_argument("--print-internal", default=False, action='store_true',
+                        help="print internal buffers")
+    parser.add_argument("--dry", default=False, action='store_true',
+                        help="dry run")
+    parser.add_argument("--save-model", default=False, action='store_true',
+                        help="save trained model to file")
+    parser.add_argument('--save-filename', default="trained_model", type=str,
+                        help="filename of the saved model")
+    return parser.parse_args()

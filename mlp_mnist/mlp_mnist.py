e5b4bac761098968765218af8887e0d3f9b13cec
lc873 <lc873@cornell.edu>
2020-04-18 11:26:18 -0400

    [mlp_mnist] flake8 clean

--- mlp_mnist/mlp_mnist.py
+++ mlp_mnist/mlp_mnist.py
@@ -12,56 +12,58 @@ import os
 sys.path.append(os.path.join(os.path.dirname(__file__), os.pardir))
 
 import torch
-from torch                import nn
-from torch.utils.data     import DataLoader
-from torchvision          import transforms
+from torch import nn
+from torch.utils.data import DataLoader
+from torchvision import transforms
 from torchvision.datasets import MNIST
 from utils import parse_model_args, train, inference, save_model
 
-#-------------------------------------------------------------------------
+# -------------------------------------------------------------------------
 # Parse command line arguments
-#-------------------------------------------------------------------------
+# -------------------------------------------------------------------------
 
 args = parse_model_args()
 
-#-------------------------------------------------------------------------
+# -------------------------------------------------------------------------
 # Prepare Dataset
-#-------------------------------------------------------------------------
+# -------------------------------------------------------------------------
 
 train_data = MNIST('./data', train=True, download=True,
                    transform=transforms.ToTensor())
-test_data  = MNIST('./data', train=False, download=True,
-                   transform=transforms.ToTensor())
+test_data = MNIST('./data', train=False, download=True,
+                  transform=transforms.ToTensor())
 
-train_loader = DataLoader(train_data, batch_size=args.batch_size, num_workers=0)
-test_loader  = DataLoader(test_data, batch_size=args.batch_size, num_workers=0)
+train_loader = DataLoader(train_data, batch_size=args.batch_size,
+                          num_workers=0)
+test_loader = DataLoader(test_data, batch_size=args.batch_size,
+                         num_workers=0)
 
-#-------------------------------------------------------------------------
+# -------------------------------------------------------------------------
 # Multilayer Preception for MNIST
-#-------------------------------------------------------------------------
+# -------------------------------------------------------------------------
+
 
 class MLPModel(nn.Module):
 
-  def __init__(self):
-      super(MLPModel, self).__init__()
+    def __init__(self):
+        super(MLPModel, self).__init__()
 
-      self.mnist = nn.Sequential \
-      (
-        nn.Linear(784, 128),
-        nn.ReLU(),
-        nn.Dropout(0.2),
-        nn.Linear(128, 64),
-        nn.ReLU(),
-        nn.Dropout(0.2),
-        nn.Linear(64, 10),
-      )
+        self.mnist = nn.Sequential(
+          nn.Linear(784, 128),
+          nn.ReLU(),
+          nn.Dropout(0.2),
+          nn.Linear(128, 64),
+          nn.ReLU(),
+          nn.Dropout(0.2),
+          nn.Linear(64, 10))
 
-  def forward(self, x):
-      return self.mnist(x.view(-1, 28 * 28))
+    def forward(self, x):
+        return self.mnist(x.view(-1, 28 * 28))
 
-#-------------------------------------------------------------------------
+# -------------------------------------------------------------------------
 # Model creation and loading
-#-------------------------------------------------------------------------
+# -------------------------------------------------------------------------
+
 
 model = MLPModel()
 
@@ -77,45 +79,39 @@ print(model)
 
 # Quit here if dry run
 if args.dry:
-    exit( 0 )
+    exit(0)
 
-#-------------------------------------------------------------------------
+# -------------------------------------------------------------------------
 # Training
-#-------------------------------------------------------------------------
+# -------------------------------------------------------------------------
 
 if args.training:
 
     optimizer = torch.optim.SGD(model.parameters(), lr=0.01)
     criterion = nn.CrossEntropyLoss()
 
-    train \
-        (
-           model,
-           train_loader,
-           optimizer,
-           criterion,
-           args
-        )
+    train(model,
+          train_loader,
+          optimizer,
+          criterion,
+          args)
 
-#-------------------------------------------------------------------------
+# -------------------------------------------------------------------------
 # Inference
-#-------------------------------------------------------------------------
+# -------------------------------------------------------------------------
 
 if args.inference:
 
     criterion = nn.CrossEntropyLoss()
 
-    inference \
-            (
-              model,
+    inference(model,
               test_loader,
               criterion,
-              args
-            )
+              args)
 
-#-------------------------------------------------------------------------
+# -------------------------------------------------------------------------
 # Model saving
-#-------------------------------------------------------------------------
+# -------------------------------------------------------------------------
 
 if args.save_model:
-    save_model( model, args.model_filename )
+    save_model(model, args.model_filename)

e9e95fbf4aeec4ee3a22a5b6655dcbd174a858d4
Adrian Sampson <adrian@radbox.org>
2020-06-09 17:06:03 -0400

    Numpy version

--- sinkhorn_wmd/sinkhorn_wmd.py
+++ sinkhorn_wmd/sinkhorn_wmd.py
@@ -1,135 +1,69 @@
-"""
-Workload template
-04/18/2020 Lin Cheng (lc873@cornell.edu)
-"""
+import numpy
+import scipy.sparse
+from scipy.spatial.distance import cdist
 
-import sys
-import os
-sys.path.append(os.path.join(os.path.dirname(__file__), os.pardir))
+# Kernel parameters.
+N_DOCS = 5000
+QUERY_IDX = 100
+LAMBDA = 1
+NUM_ITERS = 16
 
-"""
-import torch
-from torch import nn
-from torch.utils.data import DataLoader
-from torchvision import transforms
-from torchvision.datasets import MNIST
-from utils import parse_model_args, train, inference, save_model
-"""
+# Data files. (Ask Adrian for these.)
+DATA_MAT = 'data/cache-mat.npz'
+DATA_VECS = 'data/cache-vecs.npy'
 
-# -------------------------------------------------------------------------
-# Multilayer Preception for MNIST
-# -------------------------------------------------------------------------
 
-"""
-class MLPModel(nn.Module):
+def sinkhorn_wmd(r, c, vecs):
+    # I=(r > 0)
+    sel = r.squeeze() > 0
 
-    def __init__(self):
-        super(MLPModel, self).__init__()
+    # r=r(I)
+    r = r[sel].reshape(-1, 1).astype(numpy.float64)
 
-        self.mnist = nn.Sequential(nn.Linear(784, 128),
-                                   nn.ReLU(),
-                                   nn.Dropout(0.2),
-                                   nn.Linear(128, 64),
-                                   nn.ReLU(),
-                                   nn.Dropout(0.2),
-                                   nn.Linear(64, 10))
+    # M=M(I,:)
+    M = cdist(vecs[sel], vecs).astype(numpy.float64)
 
-    def forward(self, x):
-        return self.mnist(x.view(-1, 28 * 28))
-"""
+    # x=ones(lenth(r), size(c,2)) / length(r)
+    a_dim = r.shape[0]
+    b_nobs = c.shape[1]
+    x = numpy.ones((a_dim, b_nobs)) / a_dim
 
-# -------------------------------------------------------------------------
-# Workload specific command line arguments
-# -------------------------------------------------------------------------
+    # K=exp(-lambda * M)
+    K = numpy.exp(- M * LAMBDA)
+    K_div_r = K / r
+    K_T = K.T
 
-"""
-def extra_arg_parser(parser):
-    parser.add_argument('--lr', default=0.01, type=int,
-                        help="learning rate")
-"""
+    # This version uses a fixed number of iterations instead of running
+    # until convergence.
+    for it in range(NUM_ITERS):
+        print('starting iteration {}'.format(it))
+
+        u = 1.0 / x
+
+        # Here's where a better implementation is possible by doing the
+        # SDDMM thing and avoiding the dense matrix/matrix multiply. We do the
+        # slow thing for now.
+        K_T_times_u = K_T @ u
+        one_over_K_T_times_u = 1 / (K_T_times_u)
+        v = c.multiply(one_over_K_T_times_u)
+
+        x = K_div_r @ v.tocsc()
+
+    out = (u * ((K * M) @ v)).sum(axis=0)
+    return out
+
 
 if __name__ == "__main__":
-    # -------------------------------------------------------------------------
-    # Parse command line arguments
-    # -------------------------------------------------------------------------
+    # Load data.
+    vecs = numpy.load(DATA_VECS)
+    mat = scipy.sparse.load_npz(DATA_MAT)
+    mat = mat[:, :N_DOCS]  # Use a subset of the data.
 
-    """
-    args = parse_model_args(extra_arg_parser)
-    """
+    # The query vector.
+    r = numpy.asarray(mat[:, QUERY_IDX].todense()).squeeze()
 
-    # -------------------------------------------------------------------------
-    # Prepare Dataset
-    # -------------------------------------------------------------------------
+    # The kernel itself.
+    scores = sinkhorn_wmd(r, mat, vecs)
 
-    """
-    train_data = MNIST('./data', train=True, download=True,
-                       transform=transforms.ToTensor())
-    test_data = MNIST('./data', train=False, download=True,
-                      transform=transforms.ToTensor())
-
-    train_loader = DataLoader(train_data, batch_size=args.batch_size,
-                              num_workers=0)
-    test_loader = DataLoader(test_data, batch_size=args.batch_size,
-                             num_workers=0)
-    """
-
-    # -------------------------------------------------------------------------
-    # Model creation and loading
-    # -------------------------------------------------------------------------
-
-    """
-    LEARNING_RATE = args.lr
-    model = MLPModel()
-
-    optimizer = torch.optim.SGD(model.parameters(), lr=LEARNING_RATE)
-    criterion = nn.CrossEntropyLoss()
-
-    # Load pretrained model if necessary
-    if args.load_model:
-        model.load_state_dict(torch.load(args.model_filename))
-
-    # Move model to HammerBlade if using HB
-    if args.hammerblade:
-        model.to(torch.device("hammerblade"))
-
-    print(model)
-
-    # Quit here if dry run
-    if args.dry:
-        exit(0)
-    """
-
-    # -------------------------------------------------------------------------
-    # Training
-    # -------------------------------------------------------------------------
-
-    """
-    if args.training:
-
-        train(model,
-              train_loader,
-              optimizer,
-              criterion,
-              args)
-    """
-
-    # -------------------------------------------------------------------------
-    # Inference
-    # -------------------------------------------------------------------------
-
-    """
-    if args.inference:
-
-        inference(model,
-                  test_loader,
-                  criterion,
-                  args)
-    """
-
-    # -------------------------------------------------------------------------
-    # Model saving
-    # -------------------------------------------------------------------------
-    """
-    if args.save_model:
-        save_model(model, args.model_filename)
-    """
+    # Dump output.
+    numpy.savetxt('scores_out.txt', scores, fmt='%.8e')

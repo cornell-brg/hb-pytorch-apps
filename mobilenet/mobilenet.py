eb12eb34e8d664633e86593ce0a09c2eb75894f4
Bandhav Veluri <bandhav.veluri00@gmail.com>
2020-10-29 11:15:16 -0700

    Fixed the model variable for HB

--- mobilenet/mobilenet.py
+++ mobilenet/mobilenet.py
@@ -7,6 +7,6 @@ model_hb = torchvision.models.mobilenet_v2(pretrained=True, progress=True)
 model_hb.to(torch.device("hammerblade"))
 
 out = model(torch.rand(1, 3, 224, 224))
-out_hb = model(torch.rand(1, 3, 224, 224).hammerblade())
+out_hb = model_hb(torch.rand(1, 3, 224, 224).hammerblade())
 
 assert torch.allclose(out, out_hb.cpu())

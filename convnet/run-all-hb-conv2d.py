a9f5ad8b6bfab3a6a8cc1a3ef6887c54d88ef8f8
Xingyao Zhang <zhangxyleo2013@gmail.com>
2021-05-10 15:55:57 -0700

    Fixed bug

--- convnet/run-all-hb-conv2d.py
+++ convnet/run-all-hb-conv2d.py
@@ -5,20 +5,19 @@ import copy
 import subprocess
 
 # batch_size, channels, filters, image_dim, kernel_size
-conv2d_config = []
-conv2d_config.append([4, 3, 16, 32, 3])
+conv2d_config = [[4, 3, 16, 32, 3]]
 conv2d_config.append([4, 16, 16, 32, 3])
 conv2d_config.append([4, 32, 64, 16, 3])
 conv2d_config.append([4, 64, 128, 8, 3])
 
 
-conv2d_residual_config = []
-conv2d_residual_config.append([4, 16, 16, 32, 1])
+conv2d_residual_config = [[4, 16, 16, 32, 1]]
 conv2d_residual_config.append([4, 32, 64, 16, 1])
 conv2d_residual_config.append([4, 64, 128, 8, 1])
 
 # conv2d training
 for i in range(len(conv2d_config)):
+  print(i)
   name = "test_training_kernel_conv%d_hb" % i
   sh_cmd = "mkdir -p " + name
   print(sh_cmd)
@@ -26,21 +25,21 @@ for i in range(len(conv2d_config)):
   sh_cmd = "ln -s ../conv2d.py " + name + "/conv2d.py"
   print(sh_cmd)
   os.system(sh_cmd)
-  script = "(cd " + name + "; pycosim.profile conv2d.py --batch-size {} --channels {} --filters {} --image-dim {} --kernel-size {} --padding > out.std 2>&1)".format(x for x in conv2d_config[i])
+  script = "(cd " + name + "; pycosim.profile conv2d.py --batch-size {} --channels {} --filters {} --image-dim {} --kernel-size {} --padding > out.std 2>&1)".format(conv2d_config[i][0],conv2d_config[i][1],conv2d_config[i][2],conv2d_config[i][3],conv2d_config[i][4])
   with open(name + "/run.sh", 'w') as outfile:
     outfile.write(script)
   print("starting cosim job ...")
   cosim_run = subprocess.Popen(["sh", name + "/run.sh"], env=os.environ)
 
 for i in range(len(conv2d_residual_config)):
-  name = "test_training_kernel_conv%ds_hb" % i+1
+  name = "test_training_kernel_conv{}s_hb".format(i+1)
   sh_cmd = "mkdir -p " + name
   print(sh_cmd)
   os.system(sh_cmd)
   sh_cmd = "ln -s ../conv2d.py " + name + "/conv2d.py"
   print(sh_cmd)
   os.system(sh_cmd)
-  script = "(cd " + name + "; pycosim.profile conv2d.py --batch-size {} --channels {} --filters {} --image-dim {} --kernel-size {} > out.std 2>&1)".format(x for x in conv2d_residual_config[i])
+  script = "(cd " + name + "; pycosim.profile conv2d.py --batch-size {} --channels {} --filters {} --image-dim {} --kernel-size {} > out.std 2>&1)".format(conv2d_residual_config[i][0],conv2d_residual_config[i][1],conv2d_residual_config[i][2],conv2d_residual_config[i][3],conv2d_residual_config[i][4])
   with open(name + "/run.sh", 'w') as outfile:
     outfile.write(script)
   print("starting cosim job ...")
@@ -55,21 +54,21 @@ for i in range(len(conv2d_config)):
   sh_cmd = "ln -s ../conv2d_inference.py " + name + "/conv2d_inference.py"
   print(sh_cmd)
   os.system(sh_cmd)
-  script = "(cd " + name + "; pycosim.profile conv2d_inference.py --batch-size {} --channels {} --filters {} --image-dim {} --kernel-size {} --padding > out.std 2>&1)".format(x for x in conv2d_config[i])
+  script = "(cd " + name + "; pycosim.profile conv2d_inference.py --batch-size {} --channels {} --filters {} --image-dim {} --kernel-size {} --padding > out.std 2>&1)".format(conv2d_config[i][0],conv2d_config[i][1],conv2d_config[i][2],conv2d_config[i][3],conv2d_config[i][4])
   with open(name + "/run.sh", 'w') as outfile:
     outfile.write(script)
   print("starting cosim job ...")
   cosim_run = subprocess.Popen(["sh", name + "/run.sh"], env=os.environ)
 
 for i in range(len(conv2d_residual_config)):
-  name = "test_inference_kernel_conv%ds_hb" % i+1
+  name = "test_inference_kernel_conv{}s_hb".format(i+1)
   sh_cmd = "mkdir -p " + name
   print(sh_cmd)
   os.system(sh_cmd)
   sh_cmd = "ln -s ../conv2d_inference.py " + name + "/conv2d_inference.py"
   print(sh_cmd)
   os.system(sh_cmd)
-  script = "(cd " + name + "; pycosim.profile conv2d_inference.py --batch-size {} --channels {} --filters {} --image-dim {} --kernel-size {} > out.std 2>&1)".format(x for x in conv2d_residual_config[i])
+  script = "(cd " + name + "; pycosim.profile conv2d_inference.py --batch-size {} --channels {} --filters {} --image-dim {} --kernel-size {} > out.std 2>&1)".format(conv2d_residual_config[i][0],conv2d_residual_config[i][1],conv2d_residual_config[i][2],conv2d_residual_config[i][3],conv2d_residual_config[i][4])
   with open(name + "/run.sh", 'w') as outfile:
     outfile.write(script)
   print("starting cosim job ...")

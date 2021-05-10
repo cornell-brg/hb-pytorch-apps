40b9a6e62d7b9034eab8c4130e9c6a61243e5baa
Xingyao Zhang <zhangxyleo2013@gmail.com>
2021-05-09 23:30:12 -0700

    Uploading cosim workload code

---
+++ convnet/run-all-hb-inference.py
@@ -0,0 +1,42 @@
+import sys
+import os
+import json
+import copy
+import subprocess
+
+def fancy_print(route):
+  for waypoint in route:
+    print(waypoint)
+
+with open('inference_kernel.json',) as f:
+  route = json.load(f)
+
+fancy_print(route)
+print()
+
+print("total number of jobs: " + str(len(route)))
+# exit()
+
+for i in range(len(route)): # ttl 20
+  cmd = copy.deepcopy(route)
+  cmd[i]['offload'] = True
+  fancy_print(cmd)
+  print()
+  name = "test_inference_kernel_%03d_hb" % i
+  sh_cmd = "mkdir -p " + name
+  print(sh_cmd)
+  os.system(sh_cmd)
+  with open(name + "/inference_kernel.json", 'w') as outfile:
+    json.dump(cmd, outfile, indent=4, sort_keys=True)
+  sh_cmd = "ln -s ../inference_cosim.py " + name + "/inference_cosim.py"
+  print(sh_cmd)
+  os.system(sh_cmd)
+  sh_cmd = "ln -s ../data " + name + "/data"
+  print(sh_cmd)
+  os.system(sh_cmd)
+  script = "(cd " + name + "; pycosim.profile inference_cosim.py --batch-size 4 > out.std 2>&1)"
+  with open(name + "/run.sh", 'w') as outfile:
+    outfile.write(script)
+  print("starting cosim job ...")
+  cosim_run = subprocess.Popen(["sh", name + "/run.sh"], env=os.environ)
+
